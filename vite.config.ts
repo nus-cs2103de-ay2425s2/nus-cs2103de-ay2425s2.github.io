773beb27efa5bd239ef826f3c9d1895568f26e61
Zikun Zhu <zhu.zikun@u.nus.edu>
2025-01-14 03:07:10 +0800

    feat: set up GitHub Pages deployment at https://nus-cs2103de-ay2425s1.github.io/landing-page

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE || '/',
+  base: '/landing-page/',
   build: {
     outDir: 'dist',
     sourcemap: true,
+    manifest: true,
     rollupOptions: {
       output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
       }
     }
   },
