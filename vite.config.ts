ab5e52dc9cb32dfb85629a17e899be453324f130
Zikun Zhu <zhu.zikun@u.nus.edu>
2025-01-14 02:55:20 +0800

    fix: delete unused mode param

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
   base: process.env.VITE_BASE || '/',
   resolve: {
@@ -30,4 +30,4 @@ export default defineConfig(({ mode }) => ({
     strictPort: true,
     open: true,
   },
-}))
+})
