a5194066475c231ef4a52fd5915a60bc6c28007a
Zikun Zhu <zhu.zikun@u.nus.edu>
2025-01-14 03:22:51 +0800

    feat: deploy to https://nus-cs2103de-ay2425s1.github.io/ too

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/landing-page/',
+  base: process.env.DEPLOY_TARGET === 'org' ? '/' : '/landing-page/',
   build: {
     outDir: 'dist',
     sourcemap: true,
