00e6207bfe9dd12bcacd3b6bf22e8c93dae8379f
Zikun Zhu <zhu.zikun@u.nus.edu>
2025-01-14 03:44:31 +0800

    feat: trigger deployment to organization site

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.DEPLOY_TARGET === 'org' ? '/' : '/landing-page/',
+  base: '/',
   build: {
-    outDir: 'dist',
-    sourcemap: true,
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]'
-      }
-    }
+    sourcemap: true
   }
 })
